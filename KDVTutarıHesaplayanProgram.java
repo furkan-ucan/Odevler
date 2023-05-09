fa521ac3e02f348aa87b2cfa2d1a18939d7327bd
furkan-ucan <63furkan27@gmail.com>
2023-05-10 02:14:09 +0300

    KDVTutarı

---
+++ KDVTutarıHesaplayanProgram.java
@@ -0,0 +1,17 @@
+public class KDVTutarıHesaplayanProgram {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen ürünün fiyatını girin: ");
+        double fiyat = input.nextDouble();
+
+        double kdvOrani = fiyat <= 1000 ? 0.18 : 0.08;
+        double kdvTutari = fiyat * kdvOrani;
+        double kdvliFiyat = fiyat + kdvTutari;
+
+        System.out.println("KDV'siz fiyat: " + fiyat);
+        System.out.println("KDV oranı: %" + (int)(kdvOrani * 100));
+        System.out.println("KDV tutarı: " + kdvTutari);
+        System.out.println("KDV'li fiyat: " + kdvliFiyat);
+    }
+}
